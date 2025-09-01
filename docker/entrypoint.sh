51955f9d584d45d713691886276f887d5b5fc851
Gregory Favre <greg@beyondthewall.ch>
2025-09-01 17:24:42 +0200

    deps

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,10 @@ echo "⏳ Waiting for database..."
 until nc -z db 5432; do
   sleep 1
 done
-
+echo "⏳ Waiting for Redis..."
+until nc -z redis 6379; do
+  sleep 1
+done
 # Run migrations at runtime
 echo "⚙️ Applying database migrations..."
 python manage.py migrate --noinput
