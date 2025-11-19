ecfbaa82cf3df62c9867076dc6fe98de84a5e334
Gregory Favre <greg@beyondthewall.ch>
2025-11-19 10:42:03 +0100

    cleanup

--- config/celery_app.py
+++ config/celery_app.py
@@ -5,7 +5,7 @@ from celery.signals import setup_logging
 
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 app = Celery("budgetis")
 
